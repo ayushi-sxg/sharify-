740eb42df3e46899f0127a0619295365c731a11a
Ayushi Agrawal <78734857+ayushi-sxg@users.noreply.github.com>
2022-07-07 10:23:11 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+function connectDB(){
+
+  mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true,
+       useCreateIndex: true, useUnifiedTopology: true});
+
+  const connection= mongoose.connection;
+
+
+  connection.once('open', ()=>{
+    console.log('database connection')
+  }).catch(err => {
+    console.log('connection failed ')
+  })
+}
+
+module.exports= connectDB;
